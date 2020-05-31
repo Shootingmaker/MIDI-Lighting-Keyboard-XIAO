92b2fee2d040d857968dc144a137b8388b3a7139
Shootingmaker <shootingmaker@gmail.com>
2020-05-31 21:28:07 +0900

    Global Brightness
    Color setting change.
    Add Global Brightness setting.

--- Software/MIDI-Lighting-Keyboard_APA102_XIAO_octave_shift_plus_PCB_Revers/MIDI-Lighting-Keyboard_APA102_XIAO_octave_shift_plus_PCB_Revers.ino
+++ Software/MIDI-Lighting-Keyboard_APA102_XIAO_octave_shift_plus_PCB_Revers/MIDI-Lighting-Keyboard_APA102_XIAO_octave_shift_plus_PCB_Revers.ino
@@ -44,7 +44,7 @@ https://github.com/arduino-libraries/MIDIUSB
 
 -----------------------------------------------------------------------------*/
 #include <USB-MIDI.h>
-static  const  unsigned SysExMaxSize = 1024*24 ;  //1024*24K
+static  const  unsigned SysExMaxSize = 1024*28 ;  //1024*24K
 static  const  bool UseRunningStatus = true ; 
 USBMIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -59,7 +59,7 @@ int Reverse = 127+5 ;
 #define NUM_LEDS 128
 #define DATA_PIN 10
 #define CLOCK_PIN 8
-#define BRIGHTNESS  64
+#define BRIGHTNESS 24
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -80,67 +80,67 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
   } else {
     switch (channel) {  //  Select Ch       //　Blink velocity power!
       case 1:
-        leds[Reverse-pitch-octave_shift] = 0x008000;
+        leds[Reverse-pitch-octave_shift] = CRGB::Green;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=1;
         break;
       case 2:
-        leds[Reverse-pitch-octave_shift] = 0x0000FF;
+        leds[Reverse-pitch-octave_shift] = CRGB::Blue;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=2;
         break;
       case 3:
-        leds[Reverse-pitch-octave_shift] = 0xFF0000;
+        leds[Reverse-pitch-octave_shift] = CRGB::Red;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=3;
         break;
       case 4:
-        leds[Reverse-pitch-octave_shift] = 0xFFFF00;
+        leds[Reverse-pitch-octave_shift] = CRGB::Yellow;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=4;
         break;
       case 5:
-        leds[Reverse-pitch-octave_shift] = 0xFFA500;
+        leds[Reverse-pitch-octave_shift] = CRGB::Orange;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=5;
         break;
       case 6:
-        leds[Reverse-pitch-octave_shift] = 0xFF00FF;
+        leds[Reverse-pitch-octave_shift] = CRGB::Fuchsia;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=6;
         break;
       case 7:
-        leds[Reverse-pitch-octave_shift] = 0x00BFFF;
+        leds[Reverse-pitch-octave_shift] = CRGB::DeepSkyBlue;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=7;
         break;
       case 8:
-        leds[Reverse-pitch-octave_shift] = 0xFFE4E1;
+        leds[Reverse-pitch-octave_shift] = CRGB::MistyRose;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=8;
         break; 
       case 9:
-        leds[Reverse-pitch-octave_shift] = 0xADFF2F;
+        leds[Reverse-pitch-octave_shift] = CRGB::GreenYellow;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=9;
         break;
       case 10:
-        leds[Reverse-pitch-octave_shift] = 0x00008B;
+        leds[Reverse-pitch-octave_shift] = CRGB::DarkBlue;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=10;
         break;
       case 11:
-        leds[Reverse-pitch-octave_shift] = 0x00FFFF;
+        leds[Reverse-pitch-octave_shift] = CRGB::Cyan;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=11;
         break;
       case 12:
-        leds[Reverse-pitch-octave_shift] = 0xF08080;
+        leds[Reverse-pitch-octave_shift] = CRGB::LightCoral;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=12;
         break;
       case 13:
-        leds[Reverse-pitch-octave_shift] = 0x800080;
+        leds[Reverse-pitch-octave_shift] = CRGB::Purple;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=13;
         break;
       case 14:
-        leds[Reverse-pitch-octave_shift] = 0xB22222;
+        leds[Reverse-pitch-octave_shift] = CRGB::FireBrick;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=14;
         break;
       case 15:
-        leds[Reverse-pitch-octave_shift] = 0x00BFFF;
+        leds[Reverse-pitch-octave_shift] = CRGB::DeepSkyBlue;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=15;
         break;
       case 16:
-        leds[Reverse-pitch-octave_shift] = 0xF5F5F5;
+        leds[Reverse-pitch-octave_shift] = CRGB::WhiteSmoke;
         FastLEDSet_Ch[Reverse-pitch-octave_shift]=16;
         break;        
     }
@@ -208,25 +208,26 @@ void setup()
 
     
     //LED test All On! after OFF
+    FastLED.setBrightness (BRIGHTNESS);
     // -----------------------------------------------------------------------------
     FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
     //FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS); 
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot].setRGB( 20, 0, 0);
+            leds[dot].setRGB( 10, 0, 0);
             FastLED.show();
             // clear this led for the next time around the loop
             delay(delayval); // Delay for a period of time (in milliseconds).
             }
 
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot].setRGB( 0, 20, 0);
+            leds[dot].setRGB( 0, 10, 0);
             FastLED.show();
             // clear this led for the next time around the loop
             delay(delayval); // Delay for a period of time (in milliseconds).
             }
                
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot].setRGB( 0, 0, 20);
+            leds[dot].setRGB( 0, 0, 10);
             FastLED.show();
             // clear this led for the next time around the loop
             delay(delayval); // Delay for a period of time (in milliseconds).
